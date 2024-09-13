9aac4a4da954e238e40c9c260bc18475453579b7
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-13 15:55:40 -0300

    Fixes back-button bug

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,4 +1,4 @@
-<a href={{ route('home' }} class="flex gap-2 font-semibold items-center">
+<a href={{ route('home') }} class="flex gap-2 font-semibold items-center">
     <img src="{{ asset('img/arrow.png') }}" alt="arrow icon" class="h-4 rotate-180 dark:brightness-[10]" />
     <span> Back</span>
 </a>
