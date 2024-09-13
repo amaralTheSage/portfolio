828d53cf62251c694252c7fc1af5bae184427383
amaralTheSage <amaraldesouza9@gmail.com>
2024-09-13 15:44:46 -0300

    Adds landing page text

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'Home')
 
 @section('content')
-    <main class="text-black bg-[#e2e2e2] dark:text-white dark:bg-[#1a1a1a] w-full min-h-[78vh] rounded-md py-5 px-3 md:px-8">
+    <main
+        class="text-black bg-[#e2e2e2] dark:text-white dark:bg-[#1a1a1a] w-full min-h-[80vh]  rounded-md py-5 px-3 md:px-8">
         <nav class="flex justify-end items-center gap-4 light:bg-blue-300 dark:brightness-[10]">
             {{-- <a href=""><img src="{{ asset('sun.png') }}" alt="Switch color themes" class="w-8" /></a>
 
@@ -29,22 +30,26 @@
             @endauth
         </nav>
 
-        <section class="lg:w-2/5 md:w-3/5 w-[90%] font-semibold m-auto mt-28 xl:mt-32">
-            <p class="text-justify m-auto text-lg">
-                I am a student of Web Development with a couple of full stack
-                projects under the belt. I now seek to continue improving my design
-                and coding skills.
-            </p>
+        <div class="h-[70vh] flex justify-center">
+            <section class="  md:w-3/5 w-[90%]  items-center font-semibold m-auto ">
+                <p class="text-center m-auto text-xl ">
+                    I am a student of Web Development with a couple of full stack
+                    projects under the belt. I now seek to continue improving my design
+                    and coding skills.
+                </p>
 
-            <nav class="flex flex-wrap gap-x-10 gap-y-5 justify-evenly mt-8 dark:brightness-[10]">
-                <a href="{{ route('posts.index') }}" class="flex gap-2 items-center"><img src="{{ asset('img/arrow.png') }}"
-                        alt="arrow icon" class="h-4" /> my
-                    projects</a>
+                <nav class="flex flex-wrap gap-x-10 gap-y-5 justify-evenly mt-8 text-lg dark:brightness-[10]">
+                    <a href="{{ route('posts.index') }}" class="flex gap-2 items-center"><img
+                            src="{{ asset('img/arrow.png') }}" alt="arrow icon" class="h-4" /> my
+                        projects</a>
 
-                <a href="{{ route('links') }}" class="flex gap-2 items-center"><img src="{{ asset('img/arrow.png') }}"
-                        alt="arrow icon" class="h-4" /> my
-                    links</a>
-            </nav>
-        </section>
+                    <a href="{{ route('links') }}" class="flex gap-2 items-center"><img src="{{ asset('img/arrow.png') }}"
+                            alt="arrow icon" class="h-4" /> my
+                        links</a>
+                </nav>
+            </section>
+        </div>
+
+
     </main>
 @endsection
